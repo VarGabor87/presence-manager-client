7113eed6cad5daf25394b23ec2fa4151fbc02953
Varga Gabor <vargagaborszeged@gmail.com>
2018-10-08 19:02:58 +0200

    np comp changes

--- src/app/components/newpassword/newpassword.component.ts
+++ src/app/components/newpassword/newpassword.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
-
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { UserModel } from '../../models/user.model';
+import { Router } from '@angular/router';
+import { UserupdateService } from '../../services/userupdate.service';
 
 @Component({
   selector: 'app-newpassword',
@@ -8,10 +10,44 @@ import { FormGroup, FormBuilder } from '@angular/forms';
   styleUrls: ['./newpassword.component.css']
 })
 export class NewpasswordComponent implements OnInit {
+  isPasswordsMatch: Boolean;
+  userData: UserModel;
+  userUpdateData: UserModel;
 
-  constructor() {}
-
-  ngOnInit() {
+  constructor(
+    private router: Router,
+    private userupdateService: UserupdateService) {
+    this.userData = new UserModel();
+    this.userUpdateData = new UserModel();
   }
 
+  ngOnInit() {
+    this.userupdateService.isPasswordsMatch.subscribe(value => this.isPasswordsMatch = value);
+    this.userupdateService.getUserInfo().subscribe(value => {
+    this.userData = value;
+    });
+
+  }
+
+  update() {
+    if (this.userUpdateData.password !== this.userUpdateData.repeatPassword) {
+      this.userupdateService.isPasswordsMatch.next(false);
+    }
+
+    if (Object.keys(this.userUpdateData).length > 0 && this.isPasswordsMatch) {
+      this.userupdateService.update(this.userUpdateData)
+        .subscribe(
+          (response) => {
+            console.log(response);
+            this.router.navigate(['/']);
+          },
+          error => {
+            if (error === 'Bad Request') {
+              console.log(error);
+
+            }
+          }
+        );
+    }
+  }
 }
